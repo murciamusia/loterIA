60875d3e715460dc71debb358645574fcf9f8e1a
murciamusia <murciamusia@gmail.com>
2025-04-21 21:42:26 +0200

    index.js

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ bot.onText(/\/ayuda/, (msg) => {
 /participantes - Ver quién está apuntado
 /combinacion - Ver la combinación del día
 /resultados - Ver últimos resultados
+/bases
 /ayuda - Este menú
 
 ¡Gracias por usar *loterIA*! 🤖`, { parse_mode: "Markdown" });
